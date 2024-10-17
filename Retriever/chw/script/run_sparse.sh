a1096e0af14217cd671db8ff6a6072816191b3e7
pica-git0 <jilp1598.git@gmail.com>
2024-10-17 16:35:49 +0900

    chore: method 명령줄 인자 추가
    default로 bm25 추가

--- Retriever/chw/script/run_sparse.sh
+++ Retriever/chw/script/run_sparse.sh
@@ -1,3 +1,3 @@
 
 export PYTHONPATH=$PYTHONPATH:.
-python Retriever/chw/sparse_test.py --dataset_name ./data/train_dataset --data_path ./data --context_path wikipedia_documents.json --model_name_or_path bert-base-multilingual-cased --topk 100
+python Retriever/chw/sparse_test.py --dataset_name ./data/train_dataset --data_path ./data --context_path wikipedia_documents.json --model_name_or_path klue/bert-base --method bm25 --topk 10
