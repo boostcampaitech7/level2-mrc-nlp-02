2804207df81dc818c1f047824e8e5ec40c51b517
pica-git0 <jilp1598.git@gmail.com>
2024-10-17 11:06:48 +0900

    chore: script sparse test 인수 변경

--- Retriever/chw/script/run_sparse.sh
+++ Retriever/chw/script/run_sparse.sh
@@ -1,3 +1,3 @@
 
 export PYTHONPATH=$PYTHONPATH:.
-python Retriever/chw/sparse_test.py --dataset_name ./data/train_dataset --data_path ./data --context_path wikipedia_documents.json --model_name_or_path bert-base-multilingual-cased --topk 5 --use_faiss true
+python Retriever/chw/sparse_test.py --dataset_name ./data/train_dataset --data_path ./data --context_path wikipedia_documents.json --model_name_or_path bert-base-multilingual-cased --topk 100
