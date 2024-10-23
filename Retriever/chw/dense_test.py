ea11e8e467e8953a81099f2dc9d1b54d3729f027
pica-git0 <jilp1598.git@gmail.com>
2024-10-23 14:05:06 +0900

    feat: mode 인자를 설정하여 train, eval 모드에 맞게 동작하도록 구현

--- Retriever/chw/dense_test.py
+++ Retriever/chw/dense_test.py
@@ -47,6 +47,7 @@ if __name__ == "__main__":
     parser.add_argument("--topk", metavar=3, type=int, help="")
     parser.add_argument("--method", metavar="bert", type=str, help="dense embedding 모델을 인수로 전달합니다. ex) bert")
     parser.add_argument("--device", metavar="cuda", type=str, help="device를 인수로 전달합니다. ex) cuda, cpu")
+    parser.add_argument("--mode", metavar="train", type=str, help="실행 방법을 인수로 전달합니다.(새로 훈련 - train, 이미 생성한 모델 및 임베딩 활용 - eval) ex) train, eval")
     args = parser.parse_args()
 
     # Test sparse
@@ -90,9 +91,16 @@ if __name__ == "__main__":
         args=training_args, dataset=org_dataset["train"], data_path=args.data_path, context_path=args.context_path, num_neg=2, tokenizer=tokenizer, p_encoder=p_encoder, q_encoder=q_encoder
     )
 
-    #### 실행하고자 하는 모드에 따라서 train 혹은 load_model 하나를 선택하여 주석으로 칠한 후 실행 (train : 학습 후 인코더 활용, load_model : 기학습된 모델을 불러온 후 인코더 활용)
-    retriever.train()
-    # retriever.load_model()
+    #### 실행하고자 하는 모드에 따라서 train 혹은 load_model 하나를 선택하여 실행 (train : 학습 후 인코더 활용, load_model : 기학습된 모델을 불러온 후 인코더 활용)
+    if args.mode == "train":
+        retriever.train()
+    else:
+        retriever.load_model()
+
+    #### get_passage_embedding()을 실행하면 passage embedding을 실행시키거나 이미 임베딩 파일이 존재할 경우 그 파일을 불러와서 내부 변수에 저장합니다. 이는 retrieve 과정에 활용됩니다.
+    #### train mode일 경우 임베딩 파일이 존재하던지 상관 없이 다시 임베딩을 생성합니다.
+    retriever.get_passage_embedding(mode=args.mode)
+
     query = "몽골 제국의 지도자는 누구야?"
 
     with timer("single query by exhaustive search"):
