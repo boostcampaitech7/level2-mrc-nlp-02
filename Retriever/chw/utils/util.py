0eb8f4591577df7366dcd238194218a502911d9c
pica-git0 <jilp1598.git@gmail.com>
2024-10-28 00:29:35 +0900

    feat: utils retriever 결과 출력에 대해 Recall과 MRR 두 출력값을 계산하는 함수 구현

--- Retriever/chw/utils/util.py
+++ Retriever/chw/utils/util.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 import time
+import pandas as pd
 
 
 @contextmanager
@@ -7,3 +8,29 @@ def timer(name):
     t0 = time.time()
     yield
     print(f"[{name}] done in {time.time() - t0:.3f} s")
+
+
+def MRR(results: pd.DataFrame):
+
+    sum = 0
+
+    for idx, (index, row) in enumerate(results.iterrows()):
+        for context_idx, context in enumerate(row["context"]):
+            find_idx = context.find(row["original_context"])
+            if find_idx != -1:
+                sum += 1 / (context_idx + 1)
+                break
+
+    return sum / len(results)
+
+
+def Recall(results: pd.DataFrame):
+    sum = 0
+    for idx, (index, row) in enumerate(results.iterrows()):
+        for context in row["context"]:
+            find_idx = context.find(row["original_context"])
+            if find_idx != -1:
+                sum += 1
+                break
+
+    return sum / len(results)
